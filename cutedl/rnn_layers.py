64e7248f4a9d5144a1130c745806dcb51c6d49a6
brandonlyg <65598907@qq.com>
2020-05-21 22:10:16 +0800

    添加MemoryUnit,OutUnit. 完善rnn LSTM层代码

--- cutedl/rnn_layers.py
+++ cutedl/rnn_layers.py
@@ -7,6 +7,7 @@ rnn 层实现
 import pdb
 import numpy as np
 from model import Layer, LayerParam
+import activations
 
 '''
 隐藏门单元
@@ -34,8 +35,8 @@ class GateUnit(Layer):
 
         self.init_params()
 
-        self.__hstack = []
-        self.__in_batch = None
+        self.__hs= None
+        self.__in_batchs = None
 
 
     def init_params(self):
@@ -72,8 +73,14 @@ class GateUnit(Layer):
         Wh = self.__Wh.value
 
         out = in_batch @ W + hs @ Wh + b
-        self.__hstack.append(hs)
-        self.__in_batch = in_batch
+
+        if training:
+            if self.__hs is None:
+                self.__hs = []
+            if self.__in_batchs is None:
+                self.__in_batchs = []
+            self.__hstack.append(hs)
+            self.__in_batchs.append(in_batch)
 
         return self.activation(out)
 
@@ -85,10 +92,11 @@ class GateUnit(Layer):
 
         W = self.__W.value
         Wh = self.__Wh.value
-        pre_hs = self.__hstack.pop()
+        pre_hs = self.__hs.pop()
+        in_batch = self.__in_batchs.pop()
 
         grad_in_batch = grad @ W.T
-        self.__W.gradient = self.__in_batch.T @ grad
+        self.__W.gradient = in_batch.T @ grad
 
         grad_hs = grad @ Wh.T
         self.__Wh.gradient = pre_hs.T @ grad
@@ -98,46 +106,54 @@ class GateUnit(Layer):
         return grad_in_batch, grad_hs
 
     def reset(self):
-        self.__in_batch = None
-        self.__hstack = None
+        self.__in_batchs = None
+        self.__hs = None
 
         self.__W = LayerParam.reset(self.__W)
         self.__Wh = LayerParam.reset(self.__Wh)
         self.__b = LayerParam.reset(self.__b)
 
-
 '''
 记忆单元
 '''
-class MemoryUnit(Layer):
+class MemoryUnit:
 
     def __init__(self, activation='tanh'):
-        super().__init__(activation)
+        self.__activation = activations.get(activation)
 
-        self.__memories = []
-        self.__inputs = []
-        self.__mcs = []
+        self.__memories = None
+        self.__inputs = None
+        self.__mcs = None
+
+        self.__pre_memory = None
 
 
     def forward(self, forget, input, memory_choice, training):
-        pre_m = None
-        if len(self.__memories) == 0:
-            pre_m = np.zeros(forget.shape)
-        else:
-            pre_m = self.__memories[-1]
+        if self.__pre_memory is None:
+            self.__pre_memory = np.zeros(forget.shape)
 
-        cur_m = forget * pre_m + input * memory_choice
-        self.__memories.append(cur_m)
-        self.__inputs.append(inputs)
-        self.__mcs.append(memory_choice)
+        cur_m = forget * self.__pre_memory + input * memory_choice
 
-        return self.activation(cur_m)
+        if training:
+            if self.__memories is None:
+                self.__memories = []
+            if self.__inputs is None:
+                self.__inputs = []
+            if self.__mcs is None:
+                self.__mcs = []
+
+            self.__memories.append(self.__pre_memory)
+            self.__inputs.append(input)
+            self.__mcs.append(memory_choice)
+
+        self.__pre_memory = cur_m
+
+        return self.__activation(cur_m)
 
     def backward(self, gradient):
-        grad = self.activation.grad(gradient)
+        grad = self.__activation.grad(gradient)
 
-        self.__memories.pop()
-        pre_m = self.__memories[-1]
+        pre_m = self.__memories.pop()
         input = self.__inputs.pop()
         mc = self.__mcs.pop()
 
@@ -148,9 +164,45 @@ class MemoryUnit(Layer):
         return grad_forget, grad_input, grad_mc
 
     def reset(self):
-        self.__memories = []
-        self.__inputs = []
-        self.__mcs = []
+        self.__memories = None
+        self.__inputs = None
+        self.__mcs = None
+
+        self.__pre_memory = None
+
+'''
+输出单元
+'''
+class OutUnit:
+    def __init__(self):
+        self.__outs = None
+        self.__memories = None
+
+    def forward(self, out, memory, training):
+        res = out * memory
+
+        if training:
+            if self.__outs is None:
+                self.__outs = []
+            if self.__memories is None:
+                self.__memories = []
+            self.__outs.append(out)
+            self.__memories.append(memory)
+
+        return res
+
+    def backward(self, gradient):
+        out = self.__outs.pop()
+        memory = self.__memories.pop()
+
+        grad_out = gradient * memory
+        grad_memory = gradient * out
+
+        return grad_out, grad_memory
+
+    def reset(self):
+        self.__outs = None
+        self.__memories = None
 
 '''
 Long Shot-Term Memory Layer
@@ -161,8 +213,18 @@ class LSTM(Layer):
     units 输出维度
     '''
     def __int__(self, out_units, in_units=None):
+        if type(out_units) != type(1):
+            raise Exception("invalid out_units: "%str(out_units))
 
-        pass
+        self.__out_units = out_units
+
+        if in_units is not None:
+            if type(in_units) != type(1):
+                raise Exception("invalid in_units: "%str(in_units))
+            self.__in_units = in_units
+            self.__init_gates()
+
+        super().__init__()
 
     '''
     初始化门
@@ -180,24 +242,66 @@ class LSTM(Layer):
         #记忆门
         self.__g_memory = Gate(out_units, in_units, activation='tanh', parent_layer=self, layer_id=4)
 
+        #记忆单元
+        self.__memory_unit = MemoryUnit()
+        #输出单元
+        self.__out_unit = OutUnit()
+
 
     @property
     def inshape(self):
-        return self.__inshape
+        return (self.__in_units, )
 
     @property
     def outshape(self):
-        return self.__outshape
+        return (self.__out_units, )
 
     @property
     def params(self):
-        return [self.__W, self.__b]
+        res = self.__g_in.params
+        res += self.__g_forget.params
+        res += self.__g_out.params
+        res += self.__g_memory.params
+        return res
 
     def init_params(self):
         pass
 
     def set_prev(self, prev_layer):
-        pass
+        outshape = prev_layer.outshape
+        self.__in_units = outshape[-1]
+        self.__init_gates()
+        super().set_prev(prev_layer)
+
+    def __lstm_forward(self, in_batch, hs, training):
+        g_in = self.__g_in.forward(in_batch, hs, training)
+        g_forget = self.__g_forget.forward(in_batch, hs, training)
+        g_out = self.__g_out.forward(in_batch, hs, training)
+        g_memory = self.__g_memory.forward(in_batch, hs, training)
+
+        memory = self.__memory_unit.forward(g_forget, g_in, g_memory, training)
+        cur_hs = self.__out_unit.forward(g_out, memory)
+
+        return cur_hs
+
+    def __lstm_backward(self, gradient):
+        grad_out, grad_memory = self.__out_unit.backward(gradient)
+        grad_forget, grad_in, grad_gm = self.__memory_unit.backward(grad_memory)
+
+        grad_in_batch, grad_hs = self.__g_memory.backward(grad_gm)
+        tmp1, tmp2 = self.__g_out.backward(grad_out)
+        grad_in_batch += tmp1
+        grad_hs += tmp2
+
+        tmp1, tmp2 = self.__g_forget.backward(grad_forget)
+        grad_in_batch += tmp1
+        grad_hs += tmp2
+
+        tmp1, tmp2 = self.__g_forget.backward(grad_in)
+        grad_in_batch += tmp1
+        grad_hs += tmp2
+
+        return grad_in_batch, grad_hs
 
     '''
     in_batch shape=(m, t, n)
@@ -230,4 +334,11 @@ class LSTM(Layer):
         return grad_x
 
     def reset(self):
-        pass
+        self.__g_in.reset()
+        self.__g_forget.reset()
+        self.__g_out.reset()
+        self.__g_memory.reset()
+
+        self.__memory_unit.reset()
+        self.__out_unit.reset()
+        
